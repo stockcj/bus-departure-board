6b7873cadf15b6dcd4b55db9d44467a223007dbd
Christopher Stock <christopher.stock@algolia.com>
2025-09-16 09:44:31 +0100

    updated to use redis caching

--- api/departure.js
+++ api/departure.js
@@ -1,8 +1,31 @@
+import { createClient } from 'redis';
 import axios from 'axios';
 import { JSDOM } from 'jsdom';
 
+// create Redis client
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
+await redisClient.connect();
+
 export default async function handler(req, res) {
   try {
+
+    const cached = await redisClient.get(departures);
+    const now = Date.now();
+
+    if (cached) {
+        const parsed = JSON.parse(cached);
+        if (now - parsed.timestamp < 60_000) {
+            console.log("Returning cached departures");
+            return res.status(200).json(parsed.data);
+        }
+    }
+
+    console.log("Fetching fresh departures");
+
     const busStopUrl = 'https://www.cambridgeshirebus.info/Popup_Content/WebDisplay/WebDisplay.aspx?stopRef=0500SCOMB004';
     const { data } = await axios.get(busStopUrl);
 
@@ -16,8 +39,13 @@ export default async function handler(req, res) {
       const service = row.querySelector('.gridServiceItem')?.textContent.trim() || '';
       const destination = row.querySelector('.gridDestinationItem span')?.textContent.trim() || '';
       const time = row.querySelector('.gridTimeItem')?.textContent.trim() || '';
-      return { service, destination, time };
     });
+
+    await redisClient.set(
+        "departures",
+        JSON.stringify({ data: departures, timestamp: now })
+    )
+
     console.log(departures);
     res.status(200).json(departures);
   } catch (err) {
